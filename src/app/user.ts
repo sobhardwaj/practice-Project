8d2dbd9af4cdc71a1a97ef16f241e8f58eab00c1
Siddharath Shankar Bhardwaj <50177404+sobhardwaj@users.noreply.github.com>
2019-12-16 14:57:41 +0530

    add mor
    interface...example

--- src/app/user.ts
+++ src/app/user.ts
@@ -3,20 +3,36 @@ export interface IUser {
   name: String;
   position: String;
   age: Number;
+  dob: String;
   nationality: String;
 }
-export interface Company {
+export interface ICompany {
   id: Number;
   name: String;
   location: String;
   HQ: String;
-  Employee: Employee;
+  Employee: IEmployee;
 }
-export interface Employee {
+export interface IEmployee {
   id: String;
   name: String;
   place: String;
   Salary: Number;
-  company: Company;
+  package: String;
+  join_date: Date;
+  company: ICompany;
   company_id: String;
+
+  username: string;
+  totalPosts: number;
+  allPosts: Post[];
 }
+export interface ICurrentWeather {
+  city: string;
+  country: string;
+  date: Date;
+  image: string;
+  temperature: number;
+  description: string;
+}
+export interface Post {}
